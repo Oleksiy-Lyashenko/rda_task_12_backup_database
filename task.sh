d140266dbacb410222be19cbc3c6a4ea4fef1d32
oleksii_lyashenko <oleksij.layshenko@gmail.com>
2026-06-05 14:29:39 +0300

    done

--- task.sh
+++ task.sh
@@ -1,1 +1,10 @@
-#! /bin/bash
+#!/bin/bash
+
+export DB_USER=backup
+export DB_PASSWORD=P@ssw0rd
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file=ShopDBReserve_backup.sql --no-create-db
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDBReserve_backup.sql
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file=ShopDBDevelopment_backup.sql --no-create-info
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < ShopDBDevelopment_backup.sql
